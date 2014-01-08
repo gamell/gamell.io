9db0e6d8a9034793d6c87252192e29666ce40c00
Joan Gamell <gamell@gmail.com>
2014-01-09 03:19:58 +0800

    Deleting bower_components folder

--- app/bower_components/topojson/index.js
+++
@@ -1,7 +0,0 @@
-var fs = require("fs");
-
-var topojson = module.exports = new Function("topojson", "return " + fs.readFileSync(__dirname + "/topojson.js", "utf8"))();
-topojson.topology = require("./lib/topojson/topology");
-topojson.simplify = require("./lib/topojson/simplify");
-topojson.filter = require("./lib/topojson/filter");
-topojson.bind = require("./lib/topojson/bind");
